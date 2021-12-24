04170822f27bc3e58059d8bc04cc8794b2048794
EmilianoErea <92905924+EmilianoErea@users.noreply.github.com>
2021-12-24 10:44:06 -0600

    Add files via upload

---
+++ js/scroll-reveal.js
@@ -0,0 +1,13 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+  
+ScrollReveal().reveal('.content', { delay: 500 });
+ScrollReveal().reveal('.nav-menu', { delay: 500 });
+ScrollReveal().reveal('.h2-card', { delay: 500 });
+ScrollReveal().reveal('.card', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
+ScrollReveal().reveal('.main', { delay: 500 });
+ScrollReveal().reveal('.social', { delay: 500 });
+ScrollReveal().reveal('.links', { delay: 500 });
---
+++ main.js
@@ -0,0 +1,20 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+  document.querySelector(".nav-menu").classList.toggle("show");
+});
+
+  ScrollReveal().reveal('header');
+  ScrollReveal().reveal('.textos', { delay: 500 });
+  ScrollReveal().reveal('.subtitulo', { delay: 500 });
+  ScrollReveal().reveal('.boton', { delay: 500 });
+  ScrollReveal().reveal('.acerca-de',{ delay: 500});
+  ScrollReveal().reveal('.sobre-nosotros',{ delay: 500});
+  ScrollReveal().reveal('.slogan',{ delay: 500});
+  ScrollReveal().reveal('.parrafo',{ delay: 500});
+  ScrollReveal().reveal('.galeria',{ delay: 500});
+  ScrollReveal().reveal('.imagenes',{ delay: 500});
+  ScrollReveal().reveal('.imagenes-none',{ delay: 500});
+  ScrollReveal().reveal('.miembros',{ delay: 500});
+  ScrollReveal().reveal('.cliente',{ delay: 500});
+  ScrollReveal().reveal('.titulo-footer',{ delay: 500});
+  ScrollReveal().reveal('.subtitulo-footer',{ delay: 500});
+  ScrollReveal().reveal('.footer',{ delay: 500});
